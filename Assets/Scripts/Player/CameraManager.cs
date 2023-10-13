bc2c8d08763ea7016fc1b80a5e7a5eb808c00c0c
jd362 <61527381+jd362@users.noreply.github.com>
2023-10-13 12:14:59 -0400

    Added camera control to local mode scene

--- Assets/Scripts/Player/CameraManager.cs
+++ Assets/Scripts/Player/CameraManager.cs
@@ -9,13 +9,15 @@ public class CameraManager : MonoBehaviour
     public static Camera MainCamera;
     public GameObject cameraPivot;
     public GameData gameData;
+    public float cameraMoveSpeed = 0.06f;
+    public float zoomSensitivity = 0.8f;
 
 
     private Transform targetCharacter;
     public Vector3 cameraOffset;
 
     public bool cameraCentered;
-    public float ScrollSpeed = 0.8f;
+
 
     float lerpDuration = 3f;
     float timer;
@@ -30,23 +32,6 @@ public class CameraManager : MonoBehaviour
         cameraCentered = true;
         gameData = FindObjectOfType<GameData>();
 
-        /*
-        if (gameData.differentCameraView)
-        {
-            if (PhotonNetwork.LocalPlayer.ActorNumber == 1) //Dwarf photonView
-            {
-                MainCamera.transform.position = gameData.cameraViews[0];
-            }
-            else if (PhotonNetwork.LocalPlayer.ActorNumber == 2) //Giant photonView
-            {
-                MainCamera.transform.position = gameData.cameraViews[1];
-            }
-            else if (PhotonNetwork.LocalPlayer.ActorNumber == 3) //Human photonView
-            {
-                MainCamera.transform.position = gameData.cameraViews[2];
-            }
-        }
-        */
 
         //targetCharacter = GameManager.Instance.mainCharacter.transform;
         while (!PlayerMapper.Instance.Inititialized) {
@@ -60,50 +45,22 @@ public class CameraManager : MonoBehaviour
 
     private void Update()
     {
-        /*
-        if (GameManager.Instance.CurrentTurnPlayerNum != PhotonNetwork.LocalPlayer.ActorNumber) // when not the character's turn, camera can move around
-        {
-            cameraCentered = false;
-            
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
-            {
-                MainCamera.transform.position += new Vector3(Input.GetAxisRaw("Horizontal") *  0.06f, 0f, 0f);
-            }
-            else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
-            {
-                MainCamera.transform.position += new Vector3(0f, 0f, Input.GetAxisRaw("Vertical") * 0.06f);
-            }
 
-
-        }
-        else if(!cameraCentered) // when character's turn, camera move back before the character starts moving
-        {
-            timer += Time.deltaTime;
-            float t = timer / lerpDuration;
-            t = t * t * (3f - 2f * t);
-            Vector3 endPosition = targetCharacter.transform.position + cameraOffset;
-            MainCamera.transform.position = Vector3.Lerp(MainCamera.transform.position, endPosition, t);
-            if (MainCamera.transform.position == endPosition) // when camera moved back
-            {
-                cameraCentered = true;
-            }
-        }
-        */
         if (Input.GetKey(KeyCode.I)) 
         {
-            cameraPivot.transform.position += new Vector3(0f, 1 * 0.06f, 0f);
+            cameraPivot.transform.position += new Vector3(0f, 1 * cameraMoveSpeed, 0f);
         }
         if (Input.GetKey(KeyCode.K))
         {
-            cameraPivot.transform.position += new Vector3(0f, -1 * 0.06f, 0f);
+            cameraPivot.transform.position += new Vector3(0f, -1 * cameraMoveSpeed, 0f);
         }
         if (Input.GetKey(KeyCode.J))
         {
-            cameraPivot.transform.position += new Vector3(-1 * 0.03f, 0f, -1 * 0.03f);
+            cameraPivot.transform.position += new Vector3(-1 * cameraMoveSpeed/2, 0f, -1 * cameraMoveSpeed/2);
         }
         if (Input.GetKey(KeyCode.L))
         {
-            cameraPivot.transform.position += new Vector3(1 * 0.03f, 0f, 1 * 0.03f);
+            cameraPivot.transform.position += new Vector3(1 * cameraMoveSpeed/2, 0f, 1 * cameraMoveSpeed/2);
         }
 
     }
@@ -118,7 +75,7 @@ public class CameraManager : MonoBehaviour
             cameraPivot.transform.position = targetCharacter.transform.position; //+ cameraOffset;
         }
         
-        MainCamera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * ScrollSpeed;
+        MainCamera.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
         if (MainCamera.orthographicSize <= 1)
             MainCamera.orthographicSize = 1;
         if (MainCamera.orthographicSize >= 10)
