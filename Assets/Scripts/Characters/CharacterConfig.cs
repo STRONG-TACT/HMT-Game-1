20561aabc9d03b8cae9513828733f7d0d8cf8c00
Erik Harpstead <eharpste@cs.cmu.edu>
2024-05-16 11:00:02 -0400

    REFACTOR: Cleaned up CharacterConfig to remove unused values

--- Assets/Scripts/Characters/CharacterConfig.cs
+++ Assets/Scripts/Characters/CharacterConfig.cs
@@ -26,17 +26,9 @@ public class CharacterConfig : ScriptableObject {
     public Combat.Dice stoneDice;
 
     //Below here currently only used for reference.
-    [Tooltip("How many adjacent tiles they can see. Currently for reference only.")]
+    [Tooltip("How many adjacent tiles they can see.")]
     [Min(1)]
     public int sightRange; 
-
-    //TODO, Ultimately, we shouldn't need this as we should be able to determine it analytically from the sightRange
-    [Tooltip("Where to initial place the camera realtive to the character. Currently for reference only.")]
-    [Obsolete("We no longer use the cameraPosition, it is static for everyone.")]
-    public Vector3 cameraPosition; 
-    [Tooltip("The faces on the character die.")]
-    [Obsolete("We no longer use the individual die faces, die are now based on size")]
-    public int[] dieFaces = { 1, 2, 3, 4, 5, 6 };
 
     [Tooltip("The usual icon used for the character.")]
     public Sprite characterIcon;
